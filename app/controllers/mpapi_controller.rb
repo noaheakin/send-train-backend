fcd05ab5ebb9ad0a8ad20eb47c1960f298734f0d
noaheakin <noaheakin@gmail.com>
2020-11-24 10:45:43 -0800

    created MP API controller for initial MP fetch

---
+++ app/controllers/mpapi_controller.rb
@@ -0,0 +1,10 @@
+class MpapiController < ApplicationController
+
+    MP_API_KEY = ENV['200976653-c6067a9354900bf9a182c19f021612e4']
+
+    def show
+        response = RestClient.get "https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=#{params[:lat]}&lon=#{params[:lon]}&key=#{MP_API_KEY}"
+        result = JSON.parse(response)
+        render json: result
+    end
+end
