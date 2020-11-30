dfcc60982515e964f51860a27b243f1437cb4890
noaheakin <noaheakin@gmail.com>
2020-11-29 21:32:43 -0800

    added range parameter to mp fetch

--- app/controllers/mpapi_controller.rb
+++ app/controllers/mpapi_controller.rb
@@ -3,7 +3,7 @@ class MpapiController < ApplicationController
     MP_API_KEY = '200976653-c6067a9354900bf9a182c19f021612e4'
 
     def show
-        response = RestClient.get "https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=#{params[:lat]}&lon=#{params[:lon]}&maxResults=500&key=#{MP_API_KEY}"
+        response = RestClient.get "https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=#{params[:lat]}&lon=#{params[:lon]}&maxDistance=10&maxResults=500&key=#{MP_API_KEY}"
         result = JSON.parse(response)
         render json: result
     end
