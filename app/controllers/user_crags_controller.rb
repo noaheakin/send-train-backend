1c00eeb69212e38cef02b1f40276867ead3a9314
noaheakin <noaheakin@gmail.com>
2020-11-30 15:52:50 -0800

    added destroy action to user_crags controller

--- app/controllers/user_crags_controller.rb
+++ app/controllers/user_crags_controller.rb
@@ -1,2 +1,22 @@
 class UserCragsController < ApplicationController
+    def index
+        user_crags = UserCrag.all 
+        render json: user_crags
+    end
+
+    def show
+        user_crag = UserCrag.find(params[:id].to_i)
+        render json: user_crag
+    end
+
+    def destroy
+        user_crag = UserCrag.find(params[:id].to_i)
+        user_crag.destroy
+    end
+
+    def create
+        
+        user = UserCrag.create(user_id: params[:user_id].to_i, crag_id: params[:crag_id].to_i)
+        render json: user
+    end
 end
